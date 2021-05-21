33a4bb1794c865009de1b967a327bae696d7d92b
Joan Francesc <Pedro Garcia>
2021-05-21 16:16:39 +0200

    Opció 1 funciona correctament

--- Practica-2/src/Client/Client.java
+++ Practica-2/src/Client/Client.java
@@ -4,9 +4,15 @@ package Client;
 
 import java.net.*;
 import java.io.*;
-import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.lang.Thread.sleep;
 
 public class Client {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_CYAN = "\u001B[36m";
 
     private static String address = "localhost";
     private static int port = 1234;
@@ -17,7 +23,10 @@ public class Client {
 
     public static void main(String[] args) {
 
-        System.out.println ("-----| Benvingut a la Base de Dades de l'associació de jugadors de rol de Sant Esteve de les Roures |-----\n");
+        System.out.println
+            (ANSI_YELLOW +
+            "\n-----| Benvingut a la Base de Dades de l'associació de jugadors de rol de Sant Esteve de les Roures |-----\n"
+            + ANSI_RESET);
 
         for (;;) {
 
@@ -36,7 +45,7 @@ public class Client {
     }
 
     private static void printMenu() {
-        System.out.println ("Menú d'opcions:");
+        System.out.println (ANSI_CYAN + "Menú d'opcions:");
         System.out.println ("1 - Llista tots els noms de personatge.");
         System.out.println ("2 - Obté la informació d'un personatge.");
         System.out.println ("3 - Afegeix un personatge.");
@@ -47,15 +56,17 @@ public class Client {
     private static int getOption() {
         for (;;) {
             try {
+                sleep(100); // temps per no colapsar en cas de que surti missatge d'error
                 BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
-                System.out.println ("Escull una opció: ");
+                System.out.print ("Escull una opció: " + ANSI_RESET);
                 String optionStr = in.readLine();
+                System.out.println ();
                 int option = Integer.parseInt (optionStr);
                 if (0 < option && option <= 5) {
                     return option;
                 }
             } catch (Exception ex) {
-                System.err.println ("Error reading option.");
+                System.err.println (ANSI_RED + "Error reading option." + ANSI_RESET);
             }
         }
     }
@@ -65,12 +76,15 @@ public class Client {
             try {
                 dataOutputStream.writeInt(1);
                 dataOutputStream.flush();
+
                 int nchar = dataInputStream.readInt();
+                System.out.println ("Hi han " + nchar + " personatges a la Base de Dades:");
 
                 for (int i = 0; i < nchar; i++) {
                     String name = dataInputStream.readUTF();
                     System.out.println(i + 1 + ": " + name);
                 }
+                System.out.println ();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -79,7 +93,31 @@ public class Client {
     }
 
     private static void Option2 () {
+        if (openConnection()) {
+            try {
+                dataOutputStream.writeInt(2);
 
+                BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
+                System.out.print ("Escriu el nom del personatge: ");
+                String name;
+                name = in.readLine();
+                dataOutputStream.writeUTF(name);
+
+                dataOutputStream.flush();
+
+                if (dataInputStream.readBoolean()) {
+                    CharacterInfo character = CharacterInfo.fromBytes(dataInputStream.readAllBytes());
+                    System.out.println (character);
+                } else {
+                    String errorMessage = dataInputStream.readUTF();
+                    System.err.println (errorMessage);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            closeConnection();
+        }
     }
 
     private static void Option3 () {
@@ -91,7 +129,7 @@ public class Client {
     }
 
     private static void Option5 () {
-        System.out.println ("-----| Esperem que tornis aviat! |-----\n");
+        System.out.println (ANSI_RED + "-----| Esperem que tornis aviat! |-----\n" + ANSI_RESET);
         System.exit(0);
     }
 
@@ -104,6 +142,7 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.err.println ("ERROR");
         return false;
     }
 
--- Practica-2/src/Server/Server.java
+++ Practica-2/src/Server/Server.java
@@ -8,10 +8,119 @@ import java.util.concurrent.atomic.*;
 
 public class Server {
 
-    public static void main (String[] args) {
-        try {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
-        } catch (Exception e) {
+    private static int port = 1234;
+
+    private static ServerSocket serverSocket;
+    private static Socket socket;
+    private static DataInputStream  dataInputStream;
+    private static DataOutputStream dataOutputStream;
+
+    private static final String CHARACTERS_DB_NAME = "charactersDB.dat";
+    private static CharactersDB charactersDB;
+
+    public static void main (String[] args) {
+
+        try {
+            charactersDB = new CharactersDB (CHARACTERS_DB_NAME);
+            serverSocket = new ServerSocket (port);
+        } catch (IOException ex) {
+            System.err.println ("Error opening database!");
+            System.exit (-1);
+        }
+
+        System.out.println
+            (ANSI_YELLOW +
+            "\n-----| Servidor de la Base de Dades de l'associació de jugadors de rol de Sant Esteve de les Roures |-----\n"
+            + ANSI_RESET);
+
+        for (;;) {
+            openConnection();
+            getOption();
+            closeConnection();
+        }
+    }
+
+    private static void getOption () {
+        try {
+            int option = dataInputStream.readInt();
+            System.out.println ("L'usuari ha escollit la opció: " + option);
+            switch (option) {
+                case 1 -> Option1();
+                case 2 -> Option2();
+                case 3 -> Option3();
+                case 4 -> Option4();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void Option1 () {
+
+        System.out.println ("El servidor ha rebut la petició n. 1, processant...");
+
+        int nchar = charactersDB.getNumCharacters();
+
+        try {
+            dataOutputStream.writeInt(nchar);
+            for (int i = 0; i < nchar; i++) {
+                CharacterInfo character = charactersDB.readCharacterInfo (i);
+                dataOutputStream.writeUTF(character.getName());
+            }
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println (ANSI_GREEN + "La Petició ha estat processada correctament." + ANSI_RESET);
+    }
+
+    private static void Option2 () {
+
+        System.out.println ("El servidor ha rebut la petició n. 2, processant...");
+
+        try {
+            String name = dataInputStream.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println (ANSI_GREEN + "La Petició ha estat processada correctament." + ANSI_RESET);
+    }
+
+    private static void Option3 () {
+        System.out.println ("Option 3");
+    }
+
+    private static void Option4 () {
+        System.out.println ("Option 4");
+    }
+
+    private static void openConnection () {
+        System.out.println("Esperant connexió...");
+        try {
+            socket = serverSocket.accept();
+            dataInputStream  = new DataInputStream  (socket.getInputStream());
+            dataOutputStream = new DataOutputStream (socket.getOutputStream());
+            System.out.println (ANSI_GREEN + "Connexió establerta correctament." + ANSI_RESET);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void closeConnection() {
+        System.out.println ("Tancant connexió...");
+        try {
+            dataOutputStream.close();
+            dataInputStream.close();
+            socket.close();
+            System.out.println (ANSI_GREEN + "Connexió tancada correctament." + ANSI_RESET);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
