df14636e96546eb3dcbce2007f736b02f129c0f3
Joan Francesc <Pedro Garcia>
2021-05-21 20:49:28 +0200

    Practica 2 finalitzada

--- Practica-2/Client.java
+++ Practica-2/Client.java
@@ -1,6 +1,12 @@
 
+
 // Aquesta practica ha estat realitzada per l'alumne Joan Francesc Pedro Garcia
 // Practica 2 de Xarxes
+
+// ACLARAMENT: Pràctica funcional amb el codi que ens has passat pel campus, pujo
+// únicament els arxius Client.java i Server.java els quals son on està tota la feina feta.
+
+
 
 import java.net.*;
 import java.io.*;
@@ -14,11 +20,20 @@ public class Client {
 
     private static String address = "localhost";
     private static int port = 1234;
+    private static int timeout = 2000;
 
     private static Socket socket;
     private static DataInputStream  dataInputStream;
     private static DataOutputStream dataOutputStream;
 
+
+    /**
+     * 
+     * A la funció main fem un bucle infinit on cada cop es s'executa la funció printMenu()
+     * i la funció getOption(), sobre la opció escollida es fa un switch el cual executa la
+     * funció desitjada.
+     * 
+     */
     public static void main(String[] args) {
 
         System.out.println
@@ -52,6 +67,12 @@ public class Client {
         }
     }
 
+
+
+
+    /**
+     * la funció printMenu imprimeix el menú per pantalla mostrant les opcions que hi han
+     */
     private static void printMenu() {
         System.out.println ("Menú d'opcions:");
         System.out.println ("1 - Llista tots els noms de personatge.");
@@ -61,10 +82,14 @@ public class Client {
         System.out.println ("5 - Sortir.");
     }
 
+
+
+    /**
+     * La funció getOption() recull la opció escollida per l'usuari
+     */
     private static int getOption() {
         for (;;) {
             try {
-                //sleep(200); // temps per no colapsar en cas de que surti missatge d'error
                 BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
                 System.out.print ("Escull una opció: ");
                 String optionStr = in.readLine();
@@ -79,8 +104,18 @@ public class Client {
         }
     }
 
+
+
+    /**
+     * 
+     * La opció 1 és la encarregada de fer la petició a la BBDD per llistar els personatges,
+     * envia un WriteInt(1) cap a la BBDD, i la resposta rebuda es un Int amb el n. de personatges
+     * a la BBDD i tants Strings com personatges hi hagin, els quals els printa per pantalla.
+     * 
+     */
     private static void Option1 () {
         if (openConnection()) {
+            System.out.println(ANSI_GREEN + "Connexió establerta amb el servidor.\n" + ANSI_RESET);
             try {
                 dataOutputStream.writeInt(1);
                 dataOutputStream.flush();
@@ -100,6 +135,17 @@ public class Client {
         }
     }
 
+
+
+    /**
+     * 
+     * La opció 2 és la encarregada de fer la petició a la BBDD per llistar la informació d'un
+     * personatge en concret, envia a la BBDD la petició amb el nom del personatge a printar, i
+     * el servidor retorna un booleà el qual si es true vol dir que ha trobat el jugador, i si
+     * es fals que no, en cas de ser true retorna també una llista de bytes amb la informació 
+     * del personatge.
+     * 
+     */
     private static void Option2 () {
         try {
 
@@ -110,6 +156,9 @@ public class Client {
             System.out.println();
 
             if (openConnection()) {
+
+                System.out.println(ANSI_GREEN + "Connexió establerta amb el servidor.\n" + ANSI_RESET);
+
                 dataOutputStream.writeInt(2);
                 dataOutputStream.writeUTF(name);
                 dataOutputStream.flush();
@@ -128,6 +177,16 @@ public class Client {
         }
     }
 
+
+
+    /**
+     * 
+     * La opció 3 és la encarregada de fer la petició a la BBDD per crear un personatge,
+     * demanem al usuari el nom, la inteligència, la força i la constitució del personatge,
+     * i envia la informació al servidor en una llista de bytes, el servidor retorna un
+     * booleà amb true si s'ha creat el personatge i un false en cas de que ja existís.
+     * 
+     */
     private static void Option3 () {
 
         BufferedReader in = new BufferedReader (new InputStreamReader (System.in));
@@ -193,6 +252,8 @@ public class Client {
 
         try {
             if (openConnection()){
+
+                System.out.println(ANSI_GREEN + "Connexió establerta amb el servidor.\n" + ANSI_RESET);
                 System.out.println ("Afegint l'usuari a la Base de Dades...");
 
                 dataOutputStream.writeInt(3);
@@ -200,9 +261,9 @@ public class Client {
                 dataOutputStream.flush();
 
                 if (dataInputStream.readBoolean()) {
-                    System.out.println(ANSI_GREEN + "Personatge afegit correctament" + ANSI_RESET);
+                    System.out.println(ANSI_GREEN + "Personatge afegit correctament.\n" + ANSI_RESET);
                 } else {
-                    System.out.println(ANSI_RED + "Error, aquest personatge ja està a la Base de Dades." + ANSI_RESET);
+                    System.out.println(ANSI_RED + "Error, aquest personatge ja està a la Base de Dades.\n" + ANSI_RESET);
                 }
                 closeConnection();
             }
@@ -211,28 +272,87 @@ public class Client {
         }
     }
 
-    private static void Option4 () {
 
+
+    /**
+     * 
+     * La opció 4 és la encarregada de fer la petició a la BBDD per eliminar un personatge,
+     * demanem a l'usuari el nom del personatge que volem eliminar i l'enviem al servidor,
+     * el servidor ens retorna un booleà conforme si s'ha eliminat amb true o si no un false.
+     * 
+     */
+    private static void Option4 () {
+        try {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Escriu el nom del personatge: ");
+            String name;
+            name = in.readLine();
+            System.out.println();
+
+            if (openConnection()) {
+
+                System.out.println(ANSI_GREEN + "Connexió establerta amb el servidor.\n" + ANSI_RESET);
+
+                dataOutputStream.writeInt(4);
+                dataOutputStream.writeUTF(name);
+                dataOutputStream.flush();
+
+                if (dataInputStream.readBoolean()) {
+                    System.out.println(ANSI_GREEN + "Personatge eliminat correctament\n" + ANSI_RESET);
+                } else {
+                    System.out.println(ANSI_RED + "Personatge no trobat\n" + ANSI_RESET);
+                }
+                closeConnection();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+
+
+    /**
+     * 
+     * La opció 5 és la encarregada de tancar el client, amb un missatge i un exit 0 indicant
+     * que no ha hagut error.
+     * 
+     */
     private static void Option5 () {
         System.out.println (ANSI_RED + "-----| Esperem que tornis aviat! |-----\n" + ANSI_RESET);
         System.exit(0);
     }
 
-    private static boolean openConnection () { // TODO : posar "temporitzador"
+
+
+    /**
+     * 
+     * La funció openConnection() és la encarregada de fer la connexió entre client i servidor,
+     * amb un temporitzador al socket al cual si s'acaba retorna servidor acupat.
+     * 
+     */
+    private static boolean openConnection () {
         try {
+            System.out.println("Intentant connexió...");
             socket = new Socket (address, port);
+            socket.setSoTimeout(timeout);
             dataInputStream  = new DataInputStream  (socket.getInputStream());
             dataOutputStream = new DataOutputStream (socket.getOutputStream());
             return true;
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println (ANSI_RED + "Error, servidor ocupat." + ANSI_RESET);
+            return false;
         }
-        System.err.println ("ERROR");
-        return false;
     }
 
+
+
+    /**
+     * 
+     * La funció closeConnection() és la encarregada de tancar la connexió entre client i servidor.
+     * 
+     */
     private static void closeConnection() {
         try {
             dataOutputStream.close();
--- Practica-2/Server.java
+++ Practica-2/Server.java
@@ -1,9 +1,15 @@
+
+
 // Aquesta practica ha estat realitzada per l'alumne Joan Francesc Pedro Garcia
 // Practica 2 de Xarxes
 
+// ACLARAMENT: Pràctica funcional amb el codi que ens has passat pel campus, pujo
+// únicament els arxius Client.java i Server.java els quals son on està tota la feina feta.
+
+
+
 import java.net.*;
 import java.io.*;
-import java.util.concurrent.atomic.*;
 
 public class Server {
 
@@ -22,6 +28,15 @@ public class Server {
     private static final String CHARACTERS_DB_NAME = "charactersDB.dat";
     private static CharactersDB charactersDB;
 
+
+    /**
+     * 
+     * La funció main inicia la BBDD i el serverSocket per establir connexions.
+     * un cop tot iniciat, fem un bucle infinit amb el que cridem les funcions 
+     * openConnection(), getOption() i  closeConnection(), no hi ha forma de parar
+     * el programa més que matant-lo per terminal.
+     * 
+     */
     public static void main (String[] args) {
 
         try {
@@ -44,6 +59,13 @@ public class Server {
         }
     }
 
+
+
+    /**
+     * 
+     * La funció getOption() agafa  la opció que ens envia l'usuari i executa la funció corresponent.
+     * 
+     */
     private static void getOption () {
         try {
             int option = dataInputStream.readInt();
@@ -67,6 +89,15 @@ public class Server {
         }
     }
 
+
+
+    /**
+     * 
+     * La Option1() és l'encarregada de passar a l'usuari el llistat de personatges disponibles
+     * a la BBDD, el qual primer passa un int amb el numero de personatges, i seguidament el n.
+     * total de Strings necessaries amb els noms
+     * 
+     */
     private static void Option1 () {
 
         System.out.println ("El servidor ha rebut la petició n. 1, processant...");
@@ -74,6 +105,7 @@ public class Server {
         int nchar = charactersDB.getNumCharacters();
 
         try {
+            
             dataOutputStream.writeInt(nchar);
             for (int i = 0; i < nchar; i++) {
                 CharacterInfo character = charactersDB.readCharacterInfo (i);
@@ -87,6 +119,15 @@ public class Server {
         System.out.println (ANSI_GREEN + "La Petició ha estat processada correctament." + ANSI_RESET);
     }
 
+
+    
+    /**
+     * 
+     * La Option1() és l'encarregada de passar a l'usuari un personatge en concret i la seva informació,
+     * llegeix el nom del personatge i, en cas de que el personatge existeixi retorna un true i la informació
+     * del personatge amb una llista de bytes, en cas de no trobar-lo torna únicament el booleà en false.
+     * 
+     */
     private static void Option2 () {
 
         System.out.println ("El servidor ha rebut la petició n. 2, processant...");
@@ -109,8 +150,18 @@ public class Server {
         System.out.println (ANSI_GREEN + "La Petició ha estat processada correctament." + ANSI_RESET);
     }
 
+
+
+    /**
+     * 
+     * La Option3() és l'encarregada de crear un personatge nou, la qual rep una llista de bytes
+     * la cual la guarda en forma de CharacterInfo i intenta crear l'usuari, en cas de poder li 
+     * retorna s l'usuari un true i en cas contrari un false.
+     * 
+     */
     private static void Option3 () {
         System.out.println ("El servidor ha rebut la petició n. 3, processant...");
+
         try {
             CharacterInfo character = CharacterInfo.fromBytes(dataInputStream.readNBytes(CharacterInfo.SIZE));
             boolean success = charactersDB.insertNewCharacter (character);
@@ -125,12 +176,39 @@ public class Server {
         System.out.println (ANSI_GREEN + "La Petició ha estat processada correctament." + ANSI_RESET);
     }
 
+
+
+    /**
+     * 
+     * La Option4() és l'encarregada d'eliminar un personatge, la cual rep un string de l'usuari
+     * amb el nom del personatge a eliminar, en cas de que l'usuari existeixi l'elimina i retorna
+     * un booleà amb true, en cas contrari unicament retorna false a l'usuari.
+     * 
+     */
     private static void Option4 () {
         System.out.println ("El servidor ha rebut la petició n. 4, processant...");
+
+        try {
+            
+            boolean success = charactersDB.deleteByName (dataInputStream.readUTF());
+
+            dataOutputStream.writeBoolean(success);
+            dataOutputStream.flush();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         System.out.println (ANSI_GREEN + "La Petició ha estat processada correctament." + ANSI_RESET);
     }
 
+
+
+    /**
+     * 
+     * La funció openConnection() és la encarregada de fer la connexió entre client i servidor.
+     * 
+     */
     private static void openConnection () {
         System.out.println("Esperant connexió...");
         try {
@@ -143,6 +221,13 @@ public class Server {
         }
     }
 
+
+
+    /**
+     * 
+     * La funció closeConnection() és la encarregada de tancar la connexió actual entre client i servidor.
+     * 
+     */
     private static void closeConnection() {
         System.out.println ("Tancant connexió...");
         try {
